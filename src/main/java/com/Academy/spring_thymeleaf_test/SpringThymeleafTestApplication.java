0f021addfe011db0c0df02eef8b6b12b96e5c8f4
patrickwiedenmann <paalwie@outlook.de>
2024-09-13 10:34:41 +0200

    Spring Initilizer hinzugefügt

---
+++ src/main/java/com/Academy/spring_thymeleaf_test/SpringThymeleafTestApplication.java
@@ -0,0 +1,13 @@
+package com.Academy.spring_thymeleaf_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThymeleafTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Academy/spring_thymeleaf_test/SpringThymeleafTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Academy.spring_thymeleaf_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
